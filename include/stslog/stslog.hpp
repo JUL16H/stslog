97468ef52951caeca20d12919d5c1a984868f46b
ldx <17301339186@163.com>
2025-11-26 13:22:36 +0800

    fixbug

--- include/stslog/stslog.hpp
+++ include/stslog/stslog.hpp
@@ -1,52 +1,53 @@
+#pragma once
 #include "stslog/Logger.hpp"
 #include "stslog/LogRegistry.hpp"
 
 namespace stslog {
-    // template <typename... Args>
-    // void trace(std::format_string<Args...> fmt, Args&&... msgs) {
-    //     LogRegistry::instance().get_logger("default")->trace(fmt, std::forward<Args>(msgs)...);
-    // }
-    // inline void trace(const std::string& msg) {
-    //     LogRegistry::instance().get_logger("default")->trace("{}", msg);
-    // }
+    template <typename... Args>
+    inline void trace(std::format_string<Args...> fmt, Args&&... msgs) {
+        LogRegistry::instance().get_logger("default")->trace(fmt, std::forward<Args>(msgs)...);
+    }
+    inline void trace(const std::string& msg) {
+        LogRegistry::instance().get_logger("default")->trace("{}", msg);
+    }
 
-    // template <typename... Args>
-    // void debug(std::format_string<Args...> fmt, Args&&... msgs) {
-    //     LogRegistry::instance().get_logger("default")->debug(fmt, std::forward<Args>(msgs)...);
-    // }
-    // inline void debug(const std::string& msg) {
-    //     LogRegistry::instance().get_logger("default")->debug("{}", msg);
-    // }
+    template <typename... Args>
+    inline void debug(std::format_string<Args...> fmt, Args&&... msgs) {
+        LogRegistry::instance().get_logger("default")->debug(fmt, std::forward<Args>(msgs)...);
+    }
+    inline void debug(const std::string& msg) {
+        LogRegistry::instance().get_logger("default")->debug("{}", msg);
+    }
 
-    // template <typename... Args>
-    // void info(std::format_string<Args...> fmt, Args&&... msgs) {
-    //     LogRegistry::instance().get_logger("default")->info(fmt, std::forward<Args>(msgs)...);
-    // }
-    // inline void info(const std::string& msg) {
-    //     LogRegistry::instance().get_logger("default")->info("{}", msg);
-    // }
+    template <typename... Args>
+    void info(std::format_string<Args...> fmt, Args&&... msgs) {
+        LogRegistry::instance().get_logger("default")->info(fmt, std::forward<Args>(msgs)...);
+    }
+    inline void info(const std::string& msg) {
+        LogRegistry::instance().get_logger("default")->info("{}", msg);
+    }
 
-    // template <typename... Args>
-    // void warn(std::format_string<Args...> fmt, Args&&... msgs) {
-    //     LogRegistry::instance().get_logger("default")->warn(fmt, std::forward<Args>(msgs)...);
-    // }
-    // inline void warn(const std::string& msg) {
-    //     LogRegistry::instance().get_logger("default")->warn("{}", msg);
-    // }
+    template <typename... Args>
+    void warn(std::format_string<Args...> fmt, Args&&... msgs) {
+        LogRegistry::instance().get_logger("default")->warn(fmt, std::forward<Args>(msgs)...);
+    }
+    inline void warn(const std::string& msg) {
+        LogRegistry::instance().get_logger("default")->warn("{}", msg);
+    }
 
-    // template <typename... Args>
-    // void error(std::format_string<Args...> fmt, Args&&... msgs) {
-    //     LogRegistry::instance().get_logger("default")->error(fmt, std::forward<Args>(msgs)...);
-    // }
-    // inline void error(const std::string& msg) {
-    //     LogRegistry::instance().get_logger("default")->error("{}", msg);
-    // }
+    template <typename... Args>
+    void error(std::format_string<Args...> fmt, Args&&... msgs) {
+        LogRegistry::instance().get_logger("default")->error(fmt, std::forward<Args>(msgs)...);
+    }
+    inline void error(const std::string& msg) {
+        LogRegistry::instance().get_logger("default")->error("{}", msg);
+    }
 
-    // template <typename... Args>
-    // void critical(std::format_string<Args...> fmt, Args&&... msgs) {
-    //     LogRegistry::instance().get_logger("default")->critical(fmt, std::forward<Args>(msgs)...);
-    // }
-    // inline void critical(const std::string& msg) {
-    //     LogRegistry::instance().get_logger("default")->critical("{}", msg);
-    // }
+    template <typename... Args>
+    void critical(std::format_string<Args...> fmt, Args&&... msgs) {
+        LogRegistry::instance().get_logger("default")->critical(fmt, std::forward<Args>(msgs)...);
+    }
+    inline void critical(const std::string& msg) {
+        LogRegistry::instance().get_logger("default")->critical("{}", msg);
+    }
 }
